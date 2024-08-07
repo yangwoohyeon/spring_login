a6c3bf9c04dbf51fe3b53dc9f9bd7ca2739b4380
yangwoohyeon <dngus0717@gmail.com>
2024-08-08 00:07:58 +0900

    회원가입 구현중

---
+++ src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,21 @@
+package hello.login.domain.member;
+
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class Member {
+
+        private Long id;
+
+        @NotEmpty
+        private String loginid; //로그인 아이디
+
+        @NotEmpty
+        private String name;
+
+        @NotEmpty
+        private String password;
+}
