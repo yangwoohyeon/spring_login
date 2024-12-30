cc404f6d8b3c6de6c157862ae62b3edc33450b77
yangwoohyeon <dngus0717@gmail.com>
2024-12-30 23:14:18 +0900

    쿠키 사용 로그인 로그아웃 구현

--- src/main/java/hello/login/domain/login/LoginController.java
+++ src/main/java/hello/login/domain/login/LoginController.java
@@ -26,23 +26,32 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response){
-
-        if(bindingResult.hasErrors()){
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult
+            bindingResult, HttpServletResponse response) {
+        if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
-
         Member loginMember = loginService.login(form.getLoginId(),form.getPassword());
-
-        if(loginMember == null){
-            bindingResult.reject("loginFail","아이디 또는 비밀번호가 잘못됨");
+        log.info("login? {}", loginMember);
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
             return "login/loginForm";
         }
-
         //로그인 성공 처리
-
-        Cookie idCookie = new Cookie("memberId",String.valueOf(loginMember.getId()));
+        //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
         response.addCookie(idCookie);
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
